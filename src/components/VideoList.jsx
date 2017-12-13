29c0c48fa3da090f835b1c8a71a6c604eed827f3
Hack Reactor Student <student@hackreactor.com>
2017-12-13 15:06:40 -0800

    finished videoList && videoListEntry tests

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,13 +1,8 @@
-var VideoList = () => (
-  <div className="video-list">
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-  </div>
-);
-
+var VideoList = (props) => (
+	<div className="video-list">
+		{props.videos.map(video => <VideoListEntry video={video} />)}
+		</div>
+	);
 // PropTypes tell other developers what `props` a component expects
 // Warnings will be shown in the console when the defined rules are violated
 VideoList.propTypes = {
@@ -17,3 +12,4 @@ VideoList.propTypes = {
 // In the ES6 spec, files are "modules" and do not share a top-level scope.
 // `var` declarations will only exist globally where explicitly defined.
 window.VideoList = VideoList;
+
--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -1,8 +1,7 @@
 var VideoPlayer = () => (
   <div className="video-player">
     <div className="embed-responsive embed-responsive-16by9">
-      <iframe className="embed-responsive-item" src="https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1" allowFullScreen></iframe>
-    </div>
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/2Vv-BfVoq4g" frameBorder="0" gesture="media" allow="encrypted-media" allowFullScreen></iframe>    </div>
     <div className="video-player-details">
       <h3>Video Title</h3>
       <div>Video Description</div>
